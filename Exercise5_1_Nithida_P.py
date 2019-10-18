d8daaa34f441feb98745d3aa152bee94ed4a91a2
nithida <p.nithida@gmail.com>
2019-10-18 21:46:44 +0700

    Added Exercise5_1_Nithida_P.py
    Submitting homework

---
+++ Exercise5_1_Nithida_P.py
@@ -0,0 +1,6 @@
+x = int(input("First number : "))
+y = int(input("Second number : "))
+print(x," + ",y, "=",x+y)
+print(x," - ",y, "=",x-y)
+print(x," * ",y, "=",x*y)
+print(x," / ",y, "=",int(x/y))
