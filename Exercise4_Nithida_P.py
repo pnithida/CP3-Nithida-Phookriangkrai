4243743adce7d817aec7119946649a588694a84d
nithida <p.nithida@gmail.com>
2019-10-18 17:43:34 +0700

    Added Exercise4_Nithida_P.py
    Submitting homework

---
+++ Exercise4_Nithida_P.py
@@ -0,0 +1,10 @@
+FE = 95.00
+GB = 80.50
+ICS = 97.50
+CP = 84.00
+
+print("--- Your Score ---")
+print("Foundation English               :", FE)
+print("General Business                 :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming             :", CP)
