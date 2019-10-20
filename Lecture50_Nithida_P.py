e57a535cc3d133b7a8d33b88234da145cb10c1fe
nithida <p.nithida@gmail.com>
2019-10-20 14:43:47 +0700

    added Lecture50_Nithida_P
    Submitting homework
    
    x=10
    y=25

---
+++ Lecture50_Nithida_P.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(10,25)
+minusNumber(10,25)
+multiplyNumber(10,25)
+divideNumber(10,25)
